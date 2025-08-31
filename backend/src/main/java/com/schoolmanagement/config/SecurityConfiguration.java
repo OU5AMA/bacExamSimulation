d27949a67c61a73e41280325f9e9d104208da5de
Tech-Lead <lasri.oussama@gmail.com>
2025-08-31 23:59:57 +0100

    seems like that I have change something in the backend, but I can't recall exactly what was it.

---
+++ backend/src/main/java/com/schoolmanagement/config/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.schoolmanagement.config;
+
+public class SecurityConfiguration {
+}
---
+++ backend/src/main/java/com/schoolmanagement/exception/UserAlreadyExistsException.java
@@ -0,0 +1,4 @@
+package com.schoolmanagement.exception;
+
+public class UserAlreadyExistsException {
+}
